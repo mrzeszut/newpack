e1354a5d88cbab523f1c8e502294556c09721fd7
Mateusz Rzeszutek <rzeszut@agh.edu.pl>
2020-10-28 10:44:21 +0100

    poprawki

--- R/fun_pack.R
+++ R/fun_pack.R
@@ -1,12 +1,24 @@
+#' Funkcja oblicz srednia z losowych zmiennych
+#'
+#' @param a double
+#' @param b double
+#' @param d double
+#'
+#' @return double
+#'
+#' @examples
+#' fun_pack(a = 1, b = 500, d = 23)
+#' @export
+
 fun_pack <- function(a = 10, b = 20, d = 2) {
 
-  out <- sqrt(a*b*d/2 + 100+1)
+  out <- base::sqrt(a*b*d/2 + 100+1)
 
   out <- stats::rnorm(n = base::round(out,0))
   out <- base::as.data.frame(out)
-
   out <- dplyr::summarise(out,
                           mean = base::mean(out, na.rm = T))
 
   return(out)
 }
+
